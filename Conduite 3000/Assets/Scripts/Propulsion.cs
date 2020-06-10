b7c2fadf947ab304d1b16e969cc502bc940ca0a5
Antoine <antoine35.colin@gmail.com>
2020-06-10 18:25:59 +0200

    Prototype v1

---
+++ Conduite 3000/Assets/Scripts/Propulsion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propulsion : MonoBehaviour
+{
+    public float trust;
+
+    Rigidbody rigi;
+
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigi.AddForce(transform.forward * 100 * trust);
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            rigi.AddTorque(transform.forward * 100 * -Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            rigi.AddTorque(transform.right * 100 * -Input.GetAxis("Vertical"));
+        }
+    }
+}
